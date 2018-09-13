f1a44ff597d802ccd082828db98ca2f8cc472b18
Dmitrii Misharov <dmisharo@redhat.com>
2018-09-13 16:51:00 +0200

    Added Firefox in testing

--- testing/conftest.py
+++ testing/conftest.py
@@ -19,14 +19,23 @@ class CustomBrowser(Browser):
 
 
 @pytest.fixture(scope="session")
-def selenium():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.maximize_window()
+def browser_name():
+    return os.environ["BROWSER"]
+
+
+@pytest.fixture(scope="session")
+def selenium(browser_name):
+    if browser_name == "chrome":
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.headless = True
+        driver = webdriver.Chrome(options=chrome_options)
+    elif browser_name == "firefox":
+        firefox_options = webdriver.FirefoxOptions()
+        firefox_options.headless = True
+        driver = webdriver.Firefox(options=firefox_options)
     global selenium_browser
     selenium_browser = driver
     yield driver
