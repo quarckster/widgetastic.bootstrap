bf63b7ff240bd3ed19adac77e5bfe11fc8f3bbca
Dmitrii Misharov <dmisharo@redhat.com>
2018-09-14 10:15:46 +0200

    Removed allure

--- testing/conftest.py
+++ testing/conftest.py
@@ -2,7 +2,6 @@ import codecs
 import os
 import sys
 
-import allure
 import pytest
 from selenium import webdriver
 
@@ -52,10 +51,3 @@ def browser(selenium, httpserver):
         headers=[("Content-Type", "text/html")])
     selenium.get(httpserver.url)
     return CustomBrowser(selenium)
-
-
-def pytest_exception_interact(node, call, report):
-    if selenium_browser is not None:
-        allure.attach(
-            "Error screenshot", selenium_browser.get_screenshot_as_png(), allure.attach_type.PNG)
-        allure.attach("Error traceback", str(report.longrepr), allure.attach_type.TEXT)
