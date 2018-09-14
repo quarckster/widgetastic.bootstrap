2678ee9273d278ec3cb3e29bb9ab0e18f639a5c8
Dmitrii Misharov <dmisharo@redhat.com>
2018-09-14 13:56:52 +0200

    Fix travis config

--- setup.py
+++ setup.py
@@ -19,8 +19,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Quality Assurance",
